417142ca38a0287d878e46deb3478ae0b1cab3d8
Sam Wray <sam@wray.pro>
2017-04-08 20:50:11 +0100

    Add webpack, webpack-dev-server and babel-es2015

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const WebpackCleanupPlugin = require('webpack-cleanup-plugin');
+
+module.exports = {
+	entry: {
+		app: './src-app/app.js',
+		menu: './src/index.js'
+	},
+	output: {
+		filename: '[name].js',
+		path: path.resolve(__dirname, './dist')
+	},
+	resolve: {
+		alias: {
+			'nwjs-browser-menu': path.resolve(__dirname, './src/index.js')
+		}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				options: {
+					presets: [[ 'es2015', { modules: false } ]]
+				}
+			}
+		]
+	},
+	plugins: [
+		new WebpackCleanupPlugin()
+	]
+};
