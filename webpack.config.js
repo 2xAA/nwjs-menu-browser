4b2fbf1f37b89693ec4d456b45980790a0cd1cbd
Sam Wray <sam@wray.pro>
2017-04-11 13:05:03 +0100

    Updates to output commonjs compatible module

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ module.exports = {
 	},
 	output: {
 		filename: '[name].js',
-		path: path.resolve(__dirname, './dist')
+		path: path.resolve(__dirname, './dist'),
+		libraryTarget: 'commonjs-module'
 	},
 	resolve: {
 		alias: {
